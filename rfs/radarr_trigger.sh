857c133239fefbd40c7aca4e64193a41b8b2aa90
westgarrett <west.garrett@utexas.edu>
2024-09-13 22:05:33 -0500

    fixed ROOT_DIR identification

--- rfs/radarr_trigger.sh
+++ rfs/radarr_trigger.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
-ROOT_DIR="$(pwd)"
-VENV_DIR="/config/venv"
+ROOT_DIR="$(dirname ${0})"
+VENV_DIR="${ROOT_DIR}/venv"
 LOG_FILE="${ROOT_DIR}/logs/$(basename "${0}" .sh).log"
-PYTHON_ENTRY="${ROOT_DIR}/main.py"
 set -x
 
 function log() {
-    echo "$(date '+%Y-%m-%d %H:%M:%S') [$(basename ${0} .sh)]" "${1}" >> "${LOG_FILE}"
+    if [ ! -d  "${ROOT_DIR}/logs" ]
+    then
+        mkdir -p "${ROOT_DIR}/logs"
+    fi
+    log_date="$(date '+%Y-%m-%d %H:%M:%S')"
+    echo  >> "${LOG_FILE}"
+    for arg in "${@}"
+    do 
+        echo "[${log_date}] : [$(basename ${0} .sh)] : ${arg}" >> "${LOG_FILE}"
+    done
 }
 
-if [ ! -d "${VENV_DIR}" ]
-then
-    log "create a virtual environment and install requirements.txt"
-    mkdir -p ${VENV_DIR}
-    "$(which python3) -m venv ${VENV_DIR}"
-    source "${VENV_DIR}/bin/activate"
-    "$(which python3) -m pip install -r ${ROOT_DIR}/requirements.txt"
-else
-    source "${VENV_DIR}/bin/activate"
-fi
+# if [ ! -d "${VENV_DIR}" ]
+# then
+#     log "create a virtual environment and install requirements.txt"
+#     mkdir -p ${VENV_DIR}
+#     "$(which python3) -m venv ${VENV_DIR}"
+#     source "${VENV_DIR}/bin/activate"
+#     "$(which python3) -m pip install -r ${ROOT_DIR}/requirements.txt"
+# else
+#     source "${VENV_DIR}/bin/activate"
+# fi
 
 args="${@}"
 if [ -z "${radarr_eventtype}" ]
@@ -29,5 +37,5 @@ fi
 log "Radarr \"${radarr_eventtype}\" triggered"
 log "Running Python script with arguments: --callarr radarr ${args}"
 
-"$(which python3) ${PYTHON_ENTRY}" "--callarr" "radarr" ${args}
+"$(which python3)" "${PYTHON_ENTRY}" "--callarr" "radarr" ${args}
 exit 0
