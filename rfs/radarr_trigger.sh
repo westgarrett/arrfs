f5877fd271db2660f9391b625c49787e6d4b7d8a
westgarrett <west.garrett@utexas.edu>
2024-09-13 23:53:38 -0500

    testing the logging feature on remote

--- rfs/radarr_trigger.sh
+++ rfs/radarr_trigger.sh
@@ -3,29 +3,31 @@ ROOT_DIR="$(dirname ${0})"
 VENV_DIR="${ROOT_DIR}/venv"
 PYTHON_ENTRY="${ROOT_DIR}/main.py"
 LOG_FILE="${ROOT_DIR}/logs/$(basename "${0}" .sh).log"
-set -x
+# set -x
 echo "$(whoami)"
 ls -la "${ROOT_DIR}"
 
-function log_local() {
-    if [ ! -d  "${ROOT_DIR}/logs" ]
+function log() {
+    log_date="$(date '+%Y-%m-%d %H:%M:%S')"
+    log_str="[${log_date}] : [$(basename ${0} .sh)]"
+    if [ "${1}" == "true" ]
     then
-        mkdir -p "${ROOT_DIR}/logs"
+        shift
+        for arg in "${@}"
+        do 
+            echo "${log_str} : ${arg}"
+        done
+    else
+        shift
+        if [ ! -d  "${ROOT_DIR}/logs" ]
+        then
+            mkdir -p "${ROOT_DIR}/logs"
+        fi
+        for arg in "${@}"
+        do 
+            echo "${log_str} : ${arg}" >> "${LOG_FILE}"
+        done
     fi
-    log_date="$(date '+%Y-%m-%d %H:%M:%S')"
-    echo  >> "${LOG_FILE}"
-    for arg in "${@}"
-    do 
-        echo "[${log_date}] : [$(basename ${0} .sh)] : ${arg}" >> "${LOG_FILE}"
-    done
-}
-
-function log_remote() {
-    log_date="$(date '+%Y-%m-%d %H:%M:%S')"
-    for arg in "${@}"
-    do 
-        echo "[${log_date}] : [$(basename ${0} .sh)] : ${arg}"
-    done
 }
 
 function gen_venv() {
@@ -37,16 +39,15 @@ function gen_venv() {
         source "${VENV_DIR}/bin/activate"
         "$(which python3) -m pip install -r ${ROOT_DIR}/requirements.txt"
     else
+        log "venv already exists"
         source "${VENV_DIR}/bin/activate"
     fi
 }
 
-log=log_local
 remote=false
 if [ "$(whoami)" == "abc" ]
 then
-    remote=true
-    log=log_remote
+    remote="true"
 else
     gen_venv
 fi
@@ -54,11 +55,12 @@ fi
 args="${@}"
 if [ -z "${radarr_eventtype}" ]
 then
-    log "No radarr event type provided. Using \"Test\" as the default"
+    radarr_eventtype="Test"
+    log "${remote}" "No radarr event type provided. Using \"Test\" as the default"
 fi
 
-log "Radarr \"${radarr_eventtype}\" triggered"
-log "Running Python script with arguments: --callarr radarr ${args}"
+log "${remote}" "Radarr \"${radarr_eventtype}\" triggered"
+log "${remote}" "Running Python script with arguments: --callarr radarr ${args}"
 
 "$(which python3)" "${PYTHON_ENTRY}" "--callarr" "radarr" ${args}
 exit 0
