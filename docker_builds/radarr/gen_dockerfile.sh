20956bd8318a3acb16686f5ead6d303b42d3bffd
westgarrett <west.garrett@utexas.edu>
2024-09-15 18:18:02 -0500

    renamed specify_repo -> gen_dockerfile

---
+++ docker_builds/radarr/gen_dockerfile.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+script_dir="$(dirname ${0})"
+repo="https://github.com/westgarrett/rfs.git"
+branch="$(git rev-parse --abbrev-ref HEAD)"
+# branch_single=true
+branch_args=${branch_single:-true} && branch_args="--single-branch" || branch_args=""
+destination="/app/rfs"
+if [ -f "${script_dir}/Dockerfile" ]
+then
+    echo "previous Dockerfile found, removing it"
+    rm "${script_dir}/Dockerfile"
+fi
+
+cp "${script_dir}/Dockerfile.template" "${script_dir}/Dockerfile"
+
+sed -i 's|GIT_BRANCH|-b '${branch}'|g' "${script_dir}/Dockerfile"
+sed -i 's|BRANCH_SINGLE|'${branch_args}'|g' "${script_dir}/Dockerfile"
+sed -i 's|GIT_REPO|'${repo}'|g' "${script_dir}/Dockerfile"
+sed -i 's|GIT_DESTINATION|'${destination}'|g' "${script_dir}/Dockerfile"
